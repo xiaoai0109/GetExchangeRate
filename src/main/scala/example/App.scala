6fd3d7c47a4d323bba978f6369c098fa5be3ed09
xiaoai0109 <xiaoai0109@gmail.com>
2019-04-20 01:47:13 -0700

    Clean the code and regenerate the jar

--- src/main/scala/example/App.scala
+++ src/main/scala/example/App.scala
@@ -28,34 +28,22 @@ object Hello extends Greeting with App {
     val dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     val key = dateFormatter.format(new Date())
 
+    // mock up data
     val xrate =  Random.nextFloat() /1000 
-    println(xrate.toString)
     val convertedResult = f"""{\"datetime\":\"$key\",\"xrate\":${1.12460640 +xrate}%.8f}"""
     println(convertedResult)
     writeToSocket(convertedResult)
 
-    /*
-    val result = api.getData(fromCurrency, toCurrency)
-    if (!result.isEmpty()) {
-      /*
-      //      val result2 = result.replaceAll("\\s", "");
-      val result2 = result.replaceAll("\n", "");
-      println(result2)
-      //{    "Realtime Currency Exchange Rate": {        "1. From_Currency Code": "EUR",        "2. From_Currency Name": "Euro",        "3. To_Currency Code": "USD",        "4. To_Currency Name": "United States Dollar",        "5. Exchange Rate": "1.12397430",        "6. Last Refreshed": "2019-04-19 04:41:57",        "7. Time Zone": "UTC"    }}
-      //{"RealtimeCurrencyExchangeRate":{"1.From_CurrencyCode":"EUR","2.From_CurrencyName":"Euro","3.To_CurrencyCode":"USD","4.To_CurrencyName":"UnitedStatesDollar","5.ExchangeRate":"1.12765000","6.LastRefreshed":"2019-04-1016:34:02","7.TimeZone":"UTC"}}
-      writeToSocket(result2)
-*/
-      println(result)
-      val json = (new JSONObject(result)).getJSONObject("Realtime Currency Exchange Rate");
-      //      val convertedResult = s"${fromCurrency}_${toCurrency},${json.get("5. Exchange Rate")},${json.get("6. Last Refreshed")}"
-//      val result2 = s"${json.get("6. Last Refreshed")},${json.get("5. Exchange Rate")}"
-      val convertedResult = f"""{\"datetime\":\"${json.get("6. Last Refreshed")}\",\"xrate\":\"${json.get("5. Exchange Rate")}\"}"""
-
-      println(convertedResult)
-      writeToSocket(convertedResult)
-
-    }
-    */
+    // Fetch real data from api
+//    val result = api.getData(fromCurrency, toCurrency)
+//    if (!result.isEmpty()) {
+////      println(result)
+//      val json = (new JSONObject(result)).getJSONObject("Realtime Currency Exchange Rate");
+//      val convertedResult = f"""{\"datetime\":\"${json.get("6. Last Refreshed")}\",\"xrate\":\"${json.get("5. Exchange Rate")}\"}"""
+//      println(convertedResult)
+//      writeToSocket(convertedResult)
+//    }
+    
     //    Thread.sleep(60000)
     Thread.sleep(20000)
   }
