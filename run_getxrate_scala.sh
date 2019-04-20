faff14cc2470a1e9f70f7eeca2ad72958eb3806e
xiaoai0109 <xiaoai0109@gmail.com>
2019-04-20 05:44:27 -0700

    Use jar to loop get xrate

--- run_getxrate_scala.sh
+++ run_getxrate_scala.sh
@@ -1,5 +1,5 @@
 #sbt clean assembly
 #scala target/scala-2.11/GetExchangeRate-assembly-0.1.0-SNAPSHOT.jar
-#scala loop_mockup_GetExchangeRate.jar
-scala 1time_mockup_GetExchangeRate.jar
+scala loop_mockup_GetExchangeRate.jar
+#scala 1time_mockup_GetExchangeRate.jar
 #scala 1time_real_GetExchangeRate.jar
